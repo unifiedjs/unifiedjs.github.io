557e5a8ea043df04b3bd7f5efc4b1426bc6ff8a9
Titus Wormer <tituswormer@gmail.com>
2017-05-04 09:59:31 +0200

    Fix suffix on guides

--- script/markdown/plugin/suffix.js
+++ script/markdown/plugin/suffix.js
@@ -8,12 +8,12 @@ function section() {
     tree.children.push(
       h('footer', [
         h('p', [
-          'made with ❤️ by ',
+          'made by ',
           h('a', {href: 'https://github.com/wooorm'}, '@wooorm'),
           ' and ',
           h('a', {
-            href: 'https://github.com/wooorm'
-          }, 'https://github.com/unifiedjs/unifiedjs.github.io/graphs/contributors')
+            href: 'https://github.com/unifiedjs/unifiedjs.github.io/graphs/contributors'
+          }, 'contributors')
         ]),
         h('p', [
           'view the project on ',
