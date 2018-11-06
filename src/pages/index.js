6ee624212e84478885fece31866c33764ebe0954
John Otander <johnotander@gmail.com>
2018-11-06 11:22:48 -0700

    Splash page (#4)
    * Bump deps
    
    * Bump deps
    
    * Spike out what and why sections
    
    * Move around some deps

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,11 @@ import {
   Text,
   Heading,
   Button,
-  Container
+  Container,
+  Logo
 } from 'unified-ui'
+
+import Link from '../components/Link'
 
 const HERO_IMG =
   'https://c8r.imgix.net/db688dfd97f57c1949e758a1/undraw-working-late.svg'
@@ -45,5 +48,101 @@ export default () => (
         </Box>
       </Flex>
     </Container>
+
+    <Box bg="grays.0" mt={5} py={[4, 5, 6]}>
+      <Container>
+        <Heading as="h2" fontSize={5}>What is unified?</Heading>
+        <Text color="grays.8">The text processing umbrella</Text>
+        <Flex mt={[3, 4, 5]} justifyContent="space-between" flexWrap="wrap">
+          <Box pr={3} width={[1, 1, 1/3]}>
+            <Heading as="h2">Powerful</Heading>
+            <Text mt={2} mb={3} fontSize={2} color="grays.8" mb={3}>
+              There's a vibrant ecosystem that's well documented
+              to allow developers to focus on value rather than syntax.
+            </Text>
+            <Link href="/usage">Look at a demo »</Link>
+          </Box>
+          <Box px={3} width={[1, 1, 1/3]}>
+            <Heading as="h2">Extensible</Heading>
+            <Text mt={2} mb={3} fontSize={2} color="grays.8">
+              unified provides numerous plugins so you can
+              create a table of contents from your markdown,
+              linkify headings,
+              or ensure your titles adhere to Chicago-style.
+            </Text>
+            <Link href="/usage">View plugins »</Link>
+          </Box>
+          <Box pl={3} width={[1, 1, 1/3]}>
+            <Heading as="h2">Transformative</Heading>
+            <Text mt={2} mb={3} fontSize={2} color="grays.8">
+              Content is king in the modern web.
+              With unified you can stitch together content
+              in plain text, markdown, HTML, or MDX.
+            </Text>
+            <Link href="/usage">See syntaxes »</Link>
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
+
+    <Box mt={5} py={[4, 5, 6]} textAlign="center">
+      <Container>
+        <Heading as="h2" fontSize={5} mb={[3, 4, 6]}>Who uses unified?</Heading>
+
+        <Flex mt={[3, 4, 5]} justifyContent="space-between" flexWrap='wrap'>
+          <Box pr={3} width={[1, 1, 1/3]}>
+            <Logo domain="google.com" height={64} mb={3} />
+            <Box width={2/3} m='auto'>
+              <Text mt={2} mb={3} fontSize={2} color="grays.8" mb={3}>
+                <Link href="https://developers.google.com/web/fundamentals">WebFundamentals</Link> by Google uses unified to check markup and build HTML
+              </Text>
+            </Box>
+          </Box>
+          <Box px={3} width={[1, 1, 1/3]}>
+            <Logo domain="nodejs.org" height={64} mb={3}  />
+            <Box width={2/3} m='auto'>
+              <Text mt={2} mb={3} fontSize={2} color="grays.8" mb={3}>
+                <Link href="https://nodejs.org">Node</Link> uses unified to check markup in their docs
+              </Text>
+            </Box>
+          </Box>
+          <Box pl={3} width={[1, 1, 1/3]}>
+            <Logo domain="reactjs.org" height={64} mb={3} />
+            <Box width={2/3} mx='auto'>
+              <Text mt={2} mb={3} fontSize={2} color="grays.8" mb={3}>
+                <Link href="https://reactjs.org">React</Link> by Facebook uses unified through Gatsby with custom plugins to build their site
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
+
+        <Flex mt={[4, 5, 6]} justifyContent="space-between" flexWrap='wrap'>
+          <Box pr={3} width={[1, 1, 1/3]}>
+            <Logo domain="mozilla.com" height={64} mb={3} />
+            <Box width={2/3} m='auto'>
+              <Text mt={2} mb={3} fontSize={2} color="grays.8" mb={3}>
+                <Link href="https://github.com/devtools-html/debugger.html">Debugger</Link> by Mozilla uses unified to check their markup and prose
+              </Text>
+            </Box>
+          </Box>
+          <Box px={3} width={[1, 1, 1/3]}>
+            <Logo domain="gatsbyjs.com" height={64} mb={3}  />
+            <Box width={2/3} m='auto'>
+              <Text mt={2} mb={3} fontSize={2} color="grays.8" mb={3}>
+                <Link href="https://gatsbyjs.org">Gatsby</Link> uses unified to process markdown for blazing fast static site generation
+              </Text>
+            </Box>
+          </Box>
+          <Box pl={3} width={[1, 1, 1/3]}>
+            <Logo domain="github.com" height={64} mb={3} />
+            <Box width={2/3} m='auto'>
+              <Text mt={2} mb={3} fontSize={2} color="grays.8" mb={3}>
+                <Link href="https://opensource.guide">Open Source Guides</Link> by GitHub (and you) uses unified to check markup and prose style
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
   </Provider>
 )
