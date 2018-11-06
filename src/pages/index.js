605023208284801a6fd14a344dfaec6f83b9789c
John Otander <johnotander@gmail.com>
2018-11-05 17:43:00 -0700

    Update unified-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import {Provider,Flex, Box, Image, Text, Heading, Button} from 'unified-ui'
+import {Provider, Flex, Box, Image, Text, Heading, Button} from 'unified-ui'
 
 export default () => (
   <Provider>
     <>
-      <Flex maxWidth='30em' p={3} alignItems="center">
-        <Box width={[1, 1/2, 1/2]}>
+      <Flex maxWidth="30em" p={3} alignItems="center">
+        <Box width={[1, 1 / 2, 1 / 2]}>
           <Box width={['auto', 'auto', '36em']}>
             <Heading fontSize={6}>Building a bridge between formats</Heading>
-            <Text mt={2} fontSize={4}>With a vibrant ecosystem for dealing with many sources of content</Text>
+            <Text mt={2} fontSize={4} color="grays.8">
+              A vibrant ecosystem for dealing with many sources of content
+            </Text>
             <Box mt={5}>
               <Button as="a" fontSize={2} py={3} href="/introduction">
                 Learn more
               </Button>
-              <Button as="a" bg="white" color="unified" p={3} fontSize={2} href="https://opencollective.com/unified">
+              <Button
+                as="a"
+                bg="white"
+                color="unified"
+                p={3}
+                fontSize={2}
+                href="https://opencollective.com/unified"
+              >
                 Support us
               </Button>
             </Box>
           </Box>
         </Box>
-        <Box width={[1, 1/2, 1/2]}>
+        <Box width={[1, 1 / 2, 1 / 2]}>
           <Image
             maxWidth={[1, 1, 'initial']}
             src="https://c8r.imgix.net/db688dfd97f57c1949e758a1/undraw-working-late.svg"
