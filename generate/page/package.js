a446766c6c0d24e55d4023043579d89a2e6aac00
Titus Wormer <tituswormer@gmail.com>
2021-07-01 11:25:53 +0200

    Fix missing readmes

--- generate/page/package.js
+++ generate/page/package.js
@@ -3,5 +3,5 @@ import {detail} from '../component/package/detail.js'
 import {page} from './page.js'
 
 export function pkg(data, d, tree) {
-  return page(head(data, d), detail(data, d, tree))
+  return page(head(data, d), [detail(data, d, tree)])
 }
