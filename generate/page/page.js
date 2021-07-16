9547c94b2cce243dc09113c0111d77301127d539
Titus Wormer <tituswormer@gmail.com>
2021-07-16 14:29:51 +0200

    Fix empty pages

--- generate/page/page.js
+++ generate/page/page.js
@@ -4,8 +4,12 @@ export function page(heading, main) {
   return {
     type: 'root',
     children: [
-      heading && heading.length > 0 ? h('section.container', heading) : '',
-      main && main.length > 0 ? h('main.container', main) : ''
+      heading && (!Array.isArray(heading) || heading.length > 0)
+        ? h('section.container', heading)
+        : '',
+      main && (!Array.isArray(main) || main.length > 0)
+        ? h('main.container', main)
+        : ''
     ]
   }
 }
