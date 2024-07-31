a25be034f85610d15a6b7fb52d4668c5f0319121
Titus Wormer <tituswormer@gmail.com>
2024-07-31 11:49:59 +0200

    Update `css-houdini-squircle`

--- asset/houdini.js
+++ asset/houdini.js
@@ -3,8 +3,5 @@
 /* eslint-env browser */
 if ('paintWorklet' in CSS) {
   // @ts-expect-error: CSS is not yet fully typed.
-  CSS.paintWorklet.addModule(
-    // To do: update.
-    'https://www.unpkg.com/css-houdini-squircle@0.1.5/squircle.min.js'
-  )
+  CSS.paintWorklet.addModule('https://esm.sh/css-houdini-squircle@0.3?bundle')
 }
